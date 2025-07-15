553ffb0b8c2ec349a0e68c0b17c7d8bf301ab7e0
Arjunbir Singh <arjunbirsingh1699@gmail.com>
2025-07-15 21:16:50 +0530

    added utils directory for shared scripts and constants

---
+++ utils/importBloodDemand.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import csv from "csv-parser";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const MONGO_URI = 'mongodb+srv://arjunbirsingh1699:bmNgiKLA5hHbNCbr@bleedforacause.xkgwjjr.mongodb.net/importtestdb?retryWrites=true&w=majority&appName=bleedforacause';
+
+const bloodSchema = new mongoose.Schema({}, { strict: false });
+const Blood = mongoose.model('blood', bloodSchema);
+
+mongoose.connect(MONGO_URI);
+
+const results = [];
+fs.createReadStream(path.join(__dirname, '../../ai-backend/blood_demand_data.csv'))
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
+  .on('end', async () => {
+    try {
+      await Blood.insertMany(results);
+      console.log('Blood demand data import to importtestdb.blood successful!');
+      mongoose.disconnect();
+    } catch (err) {
+      console.error(err);
+      mongoose.disconnect();
+    }
+  }); 
