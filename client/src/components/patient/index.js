b564e79de1ee3e3d6b6ad64b33b9f3eb0f0307f6
Arjunbir Singh <arjunbirsingh1699@gmail.com>
2025-07-31 01:52:51 +0530

    Patient: Created entry point for patient components

---
+++ client/src/components/patient/index.js
@@ -0,0 +1,14 @@
+// Export all patient components
+import PatientDashboard from './PatientDashboardStyled';
+import PatientProfile from './PatientProfileStyled';
+import DiseasePredictor from './DiseaseStyled';
+import HaemoglobinPredictor from './HaemoglobinStyled';
+import Requests from './RequestsStyled';
+
+export {
+  PatientDashboard,
+  PatientProfile,
+  DiseasePredictor,
+  HaemoglobinPredictor,
+  Requests
+};
