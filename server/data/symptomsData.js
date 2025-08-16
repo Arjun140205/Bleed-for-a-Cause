cd2b2147252fb816bcd2e03dfd1dd38116723eb4
Arjunbir Singh <arjunbirsingh1699@gmail.com>
2025-08-16 22:54:10 +0530

    Added data folder

---
+++ server/data/symptomsData.js
@@ -0,0 +1,8 @@
+// Sample symptoms dataset
+export const commonSymptoms = [
+  "fever", "headache", "cough", "fatigue", "nausea", 
+  "vomiting", "diarrhea", "chills", "body ache", "sore throat",
+  "shortness of breath", "chest pain", "abdominal pain", "dizziness",
+  "rash", "joint pain", "weakness", "loss of appetite", "sneezing",
+  "congestion", "runny nose", "muscle pain", "sweating"
+];
